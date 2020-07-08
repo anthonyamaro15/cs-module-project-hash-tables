5a0b210f593f6bde5c741c6c488cc2a104223c9b
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-07-08 02:08:20 -0700

    passsing test

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -9,6 +9,48 @@ class HashTableEntry:
         self.next = None
 
 
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def find(self, key):
+        cur = self.head
+
+        while cur is not None:
+            if cur.key == key:
+                return cur.value
+            cur = cur.next
+
+        return None
+
+    def insert_at_head(self, key, value):
+        n = HashTableEntry(key, value)
+        n.next = self.head
+        self.head = n
+
+    def delete(self, key):
+        cur = self.head
+
+        if cur.key == key:
+            self.head = self.head.next
+            cur.next = None  # cleaning the pointer for the deleted value
+            return cur
+
+        prev = cur
+        cur = cur.next
+
+        while cur is not None:
+            if cur.key == key:
+                prev.next = cur.next
+                cur.next = None
+                return cur
+            else:
+                prev = prev.next
+                cur = cur.next
+
+        return None
+
+
 # Hash table can't have fewer than this many slots
 MIN_CAPACITY = 8
 
@@ -25,6 +67,9 @@ class HashTable:
         # Your code here
         self.capacity = [None] * capacity
       #   self.capacity = capacity
+      #   self.count = 0
+        self.num_of_items = 0
+        self.head = None
 
     def get_num_slots(self):
         """
@@ -49,7 +94,7 @@ class HashTable:
       #   load factor =  # of elements / # of buckets
       # In your terminology: the number of items currently in the table divided by the size of the array.
         # Your code here
-        return (MIN_CAPACITY / self.capacity)
+        return (self.num_of_items / self.capacity)
 
     def fnv1(self, key):
         """
@@ -83,75 +128,96 @@ class HashTable:
     def put(self, key, value):
         """
         Store the value with the given key.
-
         Hash collisions should be handled with Linked List Chaining.
-
         Implement this.
         """
         # Your code here
-      #   index = self.hash_index(key)
-        self.capacity[self.hash_index(key)] = value
+        key_index = self.hash_index(key)
+        self.num_of_items += 1
 
-      #   if self.data[index] is None:
-      #       self.data[index] = []
-      #       self.data[index].append([key, value])
+        if self.capacity[key_index] is not None:
+            # there is a linked list at the location
+            # see if key exists to override
+            overwrite = self.capacity[key_index].find(key)
+            if overwrite is not None:
+                # the key already exists in the linked list
+                cur = self.capacity[key_index].head
 
-      #   else:
-      #       self.data[index].append([key, value])
+                while cur is not None:
+                    if cur.key == key:
+                        cur.value = value
+                    cur = cur.next
+            else:
+                self.capacity[key_index].insert_at_head(key, value)
+        else:
+            # no linked list there
+            ll = LinkedList()
+            ll.insert_at_head(key, value)
+            self.capacity[key_index] = ll
 
     def delete(self, key):
         """
         Remove the value stored with the given key.
-
         Print a warning if the key is not found.
-
         Implement this.
         """
         # Your code here
+        key_index = self.hash_index(key)
 
-        self.capacity[self.hash_index(key)] = None
-      #   index = self.hash_index(key)
+        if self.capacity[key_index] is not None:
 
-      #   if self.data[index] is None:
-      #       return None
-      #   else:
-      #       self.data[index] = None
+            self.num_of_items -= 1
+            deleted_node = self.capacity[key_index].delete(key)
 
-      #   if self.data[index] is None:
-      #       print('key not found')
-      #   else:
-      #       self.put(key, None)
+            return deleted_node
+        else:
+            return None
 
     def get(self, key):
         """
         Retrieve the value stored with the given key.
-
         Returns None if the key is not found.
-
         Implement this.
         """
         # Your code here
-        return self.capacity[self.hash_index(key)]
-      #   index = self.hash_index(key)
+        key_index = self.hash_index(key)
 
-      #   if(self.data[index] is None):
-      #       return None
-      #   else:
-      #       for k in self.data[index]:
-      #           if k[0] == key:
-      #               return k[1]
+        if self.capacity[key_index] is not None:
+            return self.capacity[key_index].find(key)
+        else:
+            return None
+
+   #  def resize(self, new_capacity):
+   #      """
+   #      Changes the capacity of the hash table and
+   #      rehashes all key/value pairs.
+
+   #      Implement this.
+   #      """
+   #      # Your code here
+   #    #   new_value_array = []
+   #    #   for i in range(len(self.data)):
+   #    #       pass
 
     def resize(self, new_capacity):
         """
         Changes the capacity of the hash table and
         rehashes all key/value pairs.
-
         Implement this.
         """
         # Your code here
-      #   new_value_array = []
-      #   for i in range(len(self.data)):
-      #       pass
+        old_table = self.capacity
+        self.capacity = [None] * new_capacity
+        self.num_of_items = 0
+
+        for x in old_table:
+            if x is not None:
+                cur = x.head
+
+                while cur is not None:
+                    # do something
+                    self.put(cur.key, cur.value)
+                    cur = cur.next
 
 
 if __name__ == "__main__":
